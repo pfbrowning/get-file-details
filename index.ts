f2de4faeacc9788dba9ccf938b388afe2f5262bb
Patrick Browning III <patrick@browninglogic.com>
2024-08-08 22:55:17 -0400

    chore: move index to root & add npmignore

---
+++ index.ts
@@ -0,0 +1,2 @@
+export { getFileDetails, getAllFileDetails } from "./src/functions";
+export { FileDetail } from "./src/file-details";
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export { getFileDetails, getAllFileDetails } from "./functions";
-export { FileDetail } from "./file-details";
